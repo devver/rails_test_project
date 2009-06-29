09c5ad2bb86b6113512c844684ba3b1b8c048748
Avdi Grimm <avdi@avdi.org>
2009-06-29 12:07:38 -0600

    Added test that we're using the SQLite adapter to rails_test_project.  [#916]

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -5,4 +5,8 @@ class PostTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "that we are running on sqlite3" do
+    assert_equal "SQLite", ActiveRecord::Base.connection.adapter_name
+  end
 end
